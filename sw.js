c1bca9985545c96effc1dde35314b897707e1a7e
batarang37 <76785483+batarang37@users.noreply.github.com>
2022-02-23 22:45:02 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+self.addEventListener('install',evt=>{
+    console.log('install');
+});
+self.addEventListener('activate',evt=>{
+    console.log('activated');
+});
+self.addEventListener('fetch',evt=>{
+    console.log('fetched');
+});
